4fcc86bbb26dbfcf0d5cf87afb4f8d8be17b3f39
Abrar <78867861+Abrar-00@users.noreply.github.com>
2021-06-01 21:10:30 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "41px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
